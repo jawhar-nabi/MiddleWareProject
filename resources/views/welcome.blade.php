58044df061a3e866a10d303284ec66d72e3014b6
Skima <skimaghazi@gmail.com>
2019-12-11 12:05:37 +0100

    test commit dev

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ghazi skima laravel test
                 </div>
 
                 <div class="links">
